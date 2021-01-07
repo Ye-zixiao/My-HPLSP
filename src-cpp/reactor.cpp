5cd7659b1bfa6af228830b600823f65f3c44501c
Ye-zixiao <1461018824@qq.com>
2021-01-07 22:29:01 +0800

    date-2021-01-07

--- src-cpp/reactor.cpp
+++ src-cpp/reactor.cpp
@@ -2,160 +2,127 @@
 #include <iostream>
 #include <unordered_map>
 
-/**
- * 完全按照Reactor模式UML类图样式设计的服务器范例 
- */
 
-
-//解多路复用器（多路分发）
+//解多路复用器
 class Event_Demultiplexer {
 public:
-    Event_Demultiplexer():epfd(epoll_create(1)){}
-    ~Event_Demultiplexer() {
-        close(epfd);
-    }
+	Event_Demultiplexer():
+		epfd(epoll_create(5)){}
+	~Event_Demultiplexer() { close(epfd); }
 
-    int register_event(struct epoll_event& event) {
-        return epoll_ctl(epfd, EPOLL_CTL_ADD, event.data.fd, &event);
-    }
-    
-    int remove_event(int fd) {
-        return epoll_ctl(epfd, EPOLL_CTL_DEL, fd, nullptr);
-    }
+	int register_event(struct epoll_event& ev) {
+		return epoll_ctl(epfd, EPOLL_CTL_ADD, ev.data.fd, &ev);
+	}
 
-    template<int nums>
-    int wait(std::array<struct epoll_event, nums>& events, int timeout) {
-        return epoll_wait(epfd, events.data(), events.size(), timeout);
-    }
+	int remove_event(int fd) {
+		return epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+	}
+
+	template<int max_events>
+	int wait(std::array<struct epoll_event, max_events>& event_arr,int timeout) {
+		return epoll_wait(epfd, event_arr.data(), max_events, timeout);
+	}
 
 private:
-    int epfd;
+	int epfd;
 };
 
 
-//事件处理器，将其设置为纯虚基类纯粹是为了展示
-class EventHandler {
+//事件处理器，这里并没有采用继承的方式实现
+class Event_Handler {
 public:
-    using callback_type = void(*)(void*);
+	using callback_type = void(*)(int);
 
-    EventHandler() = default;
-    EventHandler(int fd, callback_type cb, void* da) :
-        eventfd(fd), callback(cb), data(&eventfd) {}
-    virtual ~EventHandler() = default;
+	Event_Handler(int fd,callback_type cb):
+		eventfd(fd),callback(cb){}
+	virtual ~Event_Handler() = default;
 
-    virtual void handler_event() = 0;
-    int get_handler() const { return eventfd; }
+	int get_handle() const { return eventfd; }
+	void handle_event() { callback(eventfd); }
 
 protected:
-    int eventfd;
-    callback_type callback;
-    void* data;
-};
-
-
-//I/O事件处理器
-class IOEventHandler :public EventHandler {
-public:
-    using EventHandler::EventHandler;
-    void handler_event() override { callback(data); }
-};
-
-
-//信号事件处理器
-class SignalEventHandler :public EventHandler {
-public:
-    using EventHandler::EventHandler;
-    void handler_event() override { callback(data); }
-};
-
-
-//定时事件处理器
-class TimerEventHandler :public EventHandler {
-public:
-    using EventHandler::EventHandler;
-    void handler_event() override { callback(data); }
+	int eventfd;
+	callback_type callback;
 };
 
 
 //反应器
 class Reactor {
 public:
-    
-    void register_handler(EventHandler& event) {
-        struct epoll_event ev;
-        int eventfd = event.get_handler();
+	
+	void register_handler(Event_Handler* handler) {
+		struct epoll_event ev;
+		ev.data.fd = handler->get_handle();
+		ev.events = EPOLLIN | EPOLLET;
+		demultiplexer.register_event(ev);
+		handlers[ev.data.fd] = handler;
+	}
 
-        ev.data.fd = eventfd;
-        ev.events = EPOLLIN | EPOLLET;
-        demultiplexer.register_event(ev);
-        handlers[eventfd] = &event;
-    }
 
-    void remove_handler(int fd) {
-        demultiplexer.remove_event(fd);
-        delete handlers[fd];
-        handlers.erase(fd);
-    }
+	void remove_handler(int fd) {
+		demultiplexer.remove_event(fd);
+		auto handler = handlers[fd];
+		handlers.erase(fd);
+		delete handler;
+	}
 
-    void handler_events() {
-        std::array<struct epoll_event, 10> arr;
-        int nret = demultiplexer.wait<10>(arr, -1);
-
-        for (int i = 0; i < nret; ++i) {
-            handlers[arr[i].data.fd]->handler_event();
-        }
-    }
-
+	void handle_events() {
+		std::array<struct epoll_event, 10> event_arrs;
+		for (;;) {
+			int nret = demultiplexer.wait<10>(event_arrs, -1);
+			
+			for (int i = 0; i < nret; ++i) {
+				//分发处理。对于多线程而言应该会有一个发送到请求队列的过程
+				handlers[event_arrs[i].data.fd]->handle_event();
+			}
+		}
+	}
+	
 private:
-    Event_Demultiplexer demultiplexer;
-    std::unordered_map<int, EventHandler*> handlers;
+	Event_Demultiplexer demultiplexer;
+	std::unordered_map<int, Event_Handler*> handlers;
 };
-
 
 
 static Reactor reactor;
 
-void client_func(void* args) {
-    int sockfd = *reinterpret_cast<int*>(args);
-    char buf[MBUFSIZE];
-    ssize_t nread;
+//I/O事件的回调函数
+void callback_func(int sockfd) {
+	char buf[MBUFSIZE];
+	ssize_t nread;
 
-    while ((nread = read(sockfd, buf, sizeof(buf) - 1)) > 0) {
-        buf[nread] = 0;
-        std::cout << "get " << nread << " bytes data \""
-            << buf << "\" from fd " << sockfd << std::endl;
-    }
-    if (nread == 0 || (nread == -1 && errno != EWOULDBLOCK)) {
-        reactor.remove_handler(sockfd);
-        close(sockfd);
-#ifdef DEBUG
-        std::cout << "closed fd " << sockfd << std::endl;
-#endif
-    }
+	while ((nread = read(sockfd, buf, sizeof(buf) - 1)) > 0) {
+		buf[nread] = 0;
+		std::cout << "get " << nread << " bytes data \"" << buf <<
+			"\" from fd " << sockfd << std::endl;
+	}
+	if (nread == 0 || (nread == -1 && errno != EWOULDBLOCK)) {
+		reactor.remove_handler(sockfd);
+		close(sockfd);
+		std::cout << currtime("%T") << ": connection of fd " <<
+			sockfd << " has been closed" << std::endl;
+	}
 }
 
-void accept_func(void* args) {
-    int fd = *reinterpret_cast<int*>(args);
-    struct sockaddr_in cliaddr;
-    socklen_t clilen = sizeof(cliaddr);
-
-    int connfd = accept(fd, reinterpret_cast<struct sockaddr*>(&cliaddr), &clilen);
-    std::cout << currtime("%T") << ": new connection from " << 
-        sock_ntop(reinterpret_cast<const struct sockaddr*>(&cliaddr), clilen) << std::endl;
-
-    IOEventHandler* clievent = new IOEventHandler(connfd, client_func, &connfd);
-    reactor.register_handler(*clievent);
+//监听套接字的回调函数
+void accept_func(int listenfd) {
+	struct sockaddr_in cliaddr;
+	socklen_t clilen = sizeof(cliaddr);
+	int connfd = accept(listenfd, reinterpret_cast<struct sockaddr*>(&cliaddr), &clilen);
+	std::cout << currtime("%T") << ": new connection from " <<
+		sock_ntop(reinterpret_cast<const struct sockaddr*>(&cliaddr), clilen) << std::endl;
+	reactor.register_handler(new Event_Handler(connfd, callback_func));	
 }
 
 
-int main(int argc, char* argv[]) {
-    if (argc != 2)
-        err_quit("usage: %s <serv/port>", basename(argv[0]));
 
-    int listenfd = tcp_listen(nullptr, argv[1], nullptr);
-    IOEventHandler* listenhandler = new IOEventHandler(listenfd, accept_func, &listenfd);
-    reactor.register_handler(*listenhandler);
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+		err_quit("usage: %s <serv/port>", basename(argv[0]));
 
-    for (;;)
-        reactor.handler_events();
+	int listenfd = tcp_listen(NULL, argv[1], NULL);
+	Event_Handler* acceptor = new Event_Handler(listenfd, accept_func);
+	reactor.register_handler(acceptor);
+	reactor.handle_events();
 }
